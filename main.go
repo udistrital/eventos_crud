9b31caa13f95c618253911326c514add84d1c00e
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-03-08 16:32:40 -0500

    Configuración con variables

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func init() {
-	orm.RegisterDataBase("default", "postgres", "postgres://postgres:postgres@127.0.0.1:5432/pruebaSesiones?sslmode=disable&search_path=core_new")
+	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGurls")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
 }
 
 func main() {
