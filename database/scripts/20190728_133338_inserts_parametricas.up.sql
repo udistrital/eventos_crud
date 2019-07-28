698853fad8d20f4f6c8d33bcc6e3106c42015501
JuanPiedrahita <juangonzalez1597@gmail.com>
2019-07-28 13:37:44 -0500

    #3 migration para tablas parametricas

---
+++ database/scripts/20190728_133338_inserts_parametricas.up.sql
@@ -0,0 +1,9 @@
+INSERT INTO EVENTO.ROL_ENCARGADO_EVENTO (id, nombre, descripcion, codigo_abreviacion, activo, fecha_creacion, fecha_modificacion, numero_de_orden) VALUES (1,'Creador','Creador', 'CRE', true, now(), now(), 1);
+INSERT INTO EVENTO.ROL_ENCARGADO_EVENTO (id, nombre, descripcion, codigo_abreviacion, activo, fecha_creacion, fecha_modificacion, numero_de_orden) VALUES (2,'Actualizador','Actualizador', 'act', true, now(), now(), 2);
+
+-- INSERT INTO EVENTO.TIPO_RECURRENCIA (id, nombre, descripcion, codigo_abreviacion, activo, fecha_creacion, fecha_modificacion, numero_de_orden) VALUES (1,'anual','anual', 'ano', true, now(), now(), 1);
+-- INSERT INTO EVENTO.TIPO_RECURRENCIA (id, nombre, descripcion, codigo_abreviacion, activo, fecha_creacion, fecha_modificacion, numero_de_orden) VALUES (2,'semestral','semestral', 'sem', true, now(), now(), 2);
+
+-- INSERT INTO EVENTO.TIPO_EVENTO (id, nombre, descripcion, codigo_abreviacion, activo, dependencia_id, fecha_creacion, fecha_modificacion, tipo_recurrencia_id, numero_de_orden) VALUES (1,'inscripcion','inscripcion de espacions', 'insc', true, 122, now(), now(), 1, 1);
+-- INSERT INTO EVENTO.TIPO_EVENTO (id, nombre, descripcion, codigo_abreviacion, activo, dependencia_id, fecha_creacion, fecha_modificacion, tipo_recurrencia_id, numero_de_orden) VALUES (2,'grado','grado', 'grad', true, 122, now(), now(), 2, 2);
+
